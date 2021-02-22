509a9b0260764626dcf9f56b23c07375c9151740
ksoeto <76516873+ksoeto@users.noreply.github.com>
2021-02-22 14:20:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,11 @@ first_name = player[:player.find(" ")]
 print(first_name)
 last_name_len = player[player.find(" ")+1:len (player)]
 print (last_name_len) 
-
-name_short = f'{first_name[:1]}, {last_name_len}'
+name_short = f'{first_name[:1]}. {last_name_len}'
 print(name_short)
-chant = f'{first_name}!' *len(first_name) 
-chant = chant [0:7] *3
+chant = f'{first_name}! '*len(first_name)
+chant= chant [0:8*3]
 print (chant)
-good_chant = chant !=  ','
-print(good_chant)
+good_chant = chant != ','
+print (good_chant)
 
